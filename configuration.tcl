46865d679008fc5ab9ec987e9577a2ff8e7a0b77
tRyzoNeT <55611037+tRyzoNeT@users.noreply.github.com>
2024-06-06 20:59:22 -0400

    Update configuration.tcl
    mise a jour du fichier de configuration.
    - new variable pour la database XTRA
    - Mise a jour des FLAGS pour le url.tcl

--- configuration.tcl
+++ configuration.tcl
@@ -1,47 +1,61 @@
 namespace eval ::bonaPRE {
 	array set VAR 	[list													\
-   		"release" "bonaPRE"													\
-   		"desc" "*TCL PREBOT*"												\
-   		"version" "v1P"														\
-   		"dev" "og"															\
-		"git" "https://github.com/tRyzoNeT/bonaPRE-public"					\
+   		"release" 		"bonaPRE"										\
+   		"desc" 			"*TCL PREBOT*"										\
+   		"version" 		"v1P"											\
+   		"dev" 			"og"											\
+		"git" 			"https://github.com/tRyzoNeT/bonaPRE-public"						\
 	];
 	array set mysql_  [list 												\
 		"user" 			"mysql_user"										\
 		"password" 		"mysql_pass"										\
 		"host" 			"mysql_host-ip"										\
-		"db" 			"mysql_db"											\
-		"dbmain" 		"MAIN"												\
-		"dbnuke" 		"NUKE"												\
-		"conrefresh" 	"2000"												\
+		"db" 			"mysql_db"										\
+		"dbmain" 		"MAIN"											\
+		"dbxtra" 		"XTRA"											\
+		"dbnuke" 		"NUKE"											\
+		"conrefresh" 		"2000"											\
+	];
+	array set url_  [list	 												\
+		"tvmaze"		"https://www.tvmaze.com/shows/"								\
+		"imdb"			"https://www.imdb.com/title/"								\
 	];
 	array set chan_ [list													\
-		"add"					"#bonaPRE-public"							\
-		"pred"					"#bonaPRE-public"							\
-		"nuke"					"#bonaPRE-public"							\
-		"stats"					"#bonaPRE-public"							\
-		"spam"					"#bonaPRE-public"							\
-		"filtre"				"#bonaPRE-public"							\
+		"add"			"#bonaPRE-public"									\
+		"pred"			"#bonaPRE-public"									\
+		"nuke"			"#bonaPRE-public"									\
+		"stats"			"#bonaPRE-public"									\
+		"spam"			"#bonaPRE-public"									\
+		"filtre"		"#bonaPRE-public"									\
 	];
-	array set db_ [list														\
-		"id" 					"id"										\
-		"rlsname" 				"rlsname"									\
-		"group" 				"group"										\
-		"section" 				"section"									\
-		"datetime" 				"datetime"									\
-		"lastupdated" 			"lastupdated"								\
-		"status" 				"status"									\
-		"files" 				"files"										\
-		"size" 					"size"										\
+	array set db_ [list													\
+		"id" 			"id"											\
+		"rlsname" 		"rlsname"										\
+		"group" 		"group"											\
+		"section" 		"section"										\
+		"datetime" 		"datetime"										\
+		"lastupdated" 		"lastupdated"										\
+		"status" 		"status"										\
+		"files" 		"files"											\
+		"size" 			"size"											\
 	];
 	array set nuke_ [list													\
-		"id" 					"id"										\
-		"rlsname" 				"rlsname"									\
-		"group" 				"group"										\
-		"datetime" 				"datetime"									\
-		"nuke" 					"nuke"										\
-		"raison" 				"raison"									\
-		"nukenet" 				"nukenet"									\
+		"id" 			"id"											\
+		"rlsname" 		"rlsname"										\
+		"group" 		"group"											\
+		"datetime" 		"datetime"										\
+		"nuke" 			"nuke"											\
+		"raison" 		"raison"										\
+		"nukenet" 		"nukenet"										\
+	];
+	array set xtra_ [list													\
+		"id" 			"id"											\
+		"rlsname" 		"rlsname"										\
+		"group" 		"group"											\
+		"lastupdated" 		"lastupdated"										\
+		"addurl" 		"addurl"										\
+		"imdb" 			"imdb"											\
+		"tvmaze" 		"tvmaze"										\
 	];
 	################################################################################
 	setudef flag bpadd
@@ -51,6 +65,7 @@ namespace eval ::bonaPRE {
 	setudef flag bpnuke
 	setudef flag bpsearch
 	setudef flag bpstats
+	setudef flag bpxtra
 	################################################################################
 	if { [catch { package require mysqltcl }] } { die "\[configuration.tcl - erreur\] le script nécessite le package mysqltcl." ; return }
 	package provide bonaPRE-CONF-PUBLiC 1
