bcc2a4bbf71a83e4249cdba7076b062fb60fa741
ZarTek Creole <zartek.creole@gmail.com>
2024-07-16 03:09:32 +0200

    Fix configuration

--- configuration.tcl
+++ configuration.tcl
@@ -1,87 +1,87 @@
 namespace eval ::bonaPRE {
 
   # Définition des variables générales du bot
-  array set VAR [list
-    "release"                   "bonaPRE"                                        # Nom de la release
-    "desc"                      "*TCL PREBOT*"                                   # Description du bot
-    "version"                   "v1.1P"                                          # Version du bot
-    "dev"                       "og"                                             # Développeur
-    "git"                       "https://github.com/tRyzoNeT/bonaPRE-public"     # URL du dépôt Git
+  array set VAR       [list                                                     \
+    "release"                   "bonaPRE"                                       \
+    "desc"                      "*TCL PREBOT*"                                  \
+    "version"                   "v1.1P"                                         \
+    "dev"                       "og"                                            \
+    "git"                       "https://github.com/tRyzoNeT/bonaPRE-public"    \
   ]
 
   # Configuration de la connexion MySQL
-  array set mysql_ [list
-    "user"                      "mysql_user"                                    # Utilisateur MySQL
-    "password"                  "mysql_pass"                                    # Mot de passe MySQL
-    "host"                      "mysql_host-ip"                                 # Hôte MySQL
-    "mode_ssl"                  0                                               # Utilisation du SSL (0 = non, 1 = oui)
-    "db"                        "mysql_db"                                      # Base de données
-    "dbmain"                    "MAIN"                                          # Base de données principale
-    "dburl"                     "XTRA_URL"                                      # Base de données pour les URL supplémentaires
-    "dbnuke"                    "NUKE"                                          # Base de données pour les releases nuke
-    "conrefresh"                "2000"                                          # Intervalle de rafraîchissement de la connexion
+  array set mysql_    [list                                                     \
+    "user"                      "mysql_user"                                    \
+    "password"                  "mysql_pass"                                    \
+    "host"                      "mysql_host-ip"                                 \
+    "mode_ssl"                  0                                               \
+    "db"                        "mysql_db"                                      \
+    "dbmain"                    "MAIN"                                          \
+    "dburl"                     "XTRA_URL"                                      \
+    "dbnuke"                    "NUKE"                                          \
+    "conrefresh"                "2000"                                          \
   ]
 
   # URL pour les recherches de séries et films
-  array set url_ [list
-    "tvmaze"                    "https://www.tvmaze.com/shows/"                 # URL pour TVMaze
-    "imdb"                      "https://www.imdb.com/title/"                   # URL pour IMDb
+  array set url_    [list                                                       \
+    "tvmaze"                    "https://www.tvmaze.com/shows/"                 \
+    "imdb"                      "https://www.imdb.com/title/"                   \
   ]
 
   # Configuration des canaux IRC
-  array set chan_ [list
-    "add"                       "#bonaPRE-public"                               # Canal pour les ajouts
-    "pred"                      "#bonaPRE-public"                               # Canal pour les prédictions
-    "nuke"                      "#bonaPRE-public"                               # Canal pour les nuke
-    "stats"                     "#bonaPRE-public"                               # Canal pour les statistiques
-    "spam"                      "#bonaPRE-public"                               # Canal pour les spams
-    "filtre"                    "#bonaPRE-public"                               # Canal pour les filtres
+  array set chan_   [list                                                       \
+    "add"                       "#bonaPRE-public"                               \
+    "pred"                      "#bonaPRE-public"                               \
+    "nuke"                      "#bonaPRE-public"                               \
+    "stats"                     "#bonaPRE-public"                               \
+    "spam"                      "#bonaPRE-public"                               \
+    "filtre"                    "#bonaPRE-public"                               \
   ]
 
   # Configuration des champs de la base de données
-  array set db_ [list
-    "id"                        "id"                                            # Identifiant
-    "rlsname"                   "rlsname"                                       # Nom de la release
-    "group"                     "group"                                         # Groupe
-    "section"                   "section"                                       # Section
-    "datetime"                  "datetime"                                      # Date et heure
-    "lastupdated"               "lastupdated"                                   # Dernière mise à jour
-    "status"                    "status"                                        # Statut
-    "files"                     "files"                                         # Fichiers
-    "size"                      "size"                                          # Taille
+  array set db_     [list                                                       \
+    "id"                        "id"                                            \
+    "rlsname"                   "rlsname"                                       \
+    "group"                     "group"                                         \
+    "section"                   "section"                                       \
+    "datetime"                  "datetime"                                      \
+    "lastupdated"               "lastupdated"                                   \
+    "status"                    "status"                                        \
+    "files"                     "files"                                         \
+    "size"                      "size"                                          \
   ]
 
   # Configuration des champs pour les releases nuke
-  array set nuke_ [list
-    "id"                        "id"                                            # Identifiant
-    "rlsname"                   "rlsname"                                       # Nom de la release
-    "group"                     "group"                                         # Groupe
-    "datetime"                  "datetime"                                      # Date et heure
-    "nuke"                      "nuke"                                          # Statut du nuke
-    "raison"                    "raison"                                        # Raison du nuke
-    "nukenet"                   "nukenet"                                       # Réseau de nuke
+  array set nuke_   [list                                                       \
+    "id"                        "id"                                            \
+    "rlsname"                   "rlsname"                                       \
+    "group"                     "group"                                         \
+    "datetime"                  "datetime"                                      \
+    "nuke"                      "nuke"                                          \
+    "raison"                    "raison"                                        \
+    "nukenet"                   "nukenet"                                       \
   ]
 
   # Configuration des champs pour les suppressions
-  array set del_ [list
-    "id"                        "id"                                            # Identifiant
-    "releaseName"               "releaseName"                                   # Nom de la release
-    "groupName"                 "groupName"                                     # Nom du groupe
-    "datetime"                  "datetime"                                      # Date et heure
-    "typeName"                  "typeName"                                      # Type
-    "reasonMessage"             "reasonMessage"                                 # Message de la raison
-    "delNetName"                "delNetName"                                    # Réseau de suppression
+  array set del_    [list                                                       \
+    "id"                        "id"                                            \
+    "releaseName"               "releaseName"                                   \
+    "groupName"                 "groupName"                                     \
+    "datetime"                  "datetime"                                      \
+    "typeName"                  "typeName"                                      \
+    "reasonMessage"             "reasonMessage"                                 \
+    "delNetName"                "delNetName"                                    \
   ]
 
   # Configuration des champs pour les URL de la base de données
-  array set dburl_ [list
-    "id"                        "id"                                            # Identifiant
-    "rlsname"                   "rlsname"                                       # Nom de la release
-    "group"                     "group"                                         # Groupe
-    "lastupdated"               "lastupdated"                                   # Dernière mise à jour
-    "addurl"                    "addurl"                                        # URL ajoutée
-    "imdb"                      "imdb"                                          # URL IMDb
-    "tvmaze"                    "tvmaze"                                        # URL TVMaze
+  array set dburl_  [list                                                       \
+    "id"                        "id"                                            \
+    "rlsname"                   "rlsname"                                       \
+    "group"                     "group"                                         \
+    "lastupdated"               "lastupdated"                                   \
+    "addurl"                    "addurl"                                        \
+    "imdb"                      "imdb"                                          \
+    "tvmaze"                    "tvmaze"                                        \
   ]
 
   # Définition des flags
